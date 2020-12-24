9da8c3114c67729a5b808de0e59ae20f5e29762d
caolei <caolei6767@gmail.com>
2020-12-24 23:36:45 +0800

    增加 Soundex 功能代码测试文件

---
+++ test/Catch2/TestSoundex.cpp
@@ -0,0 +1,17 @@
+#include "catch2/catch.hpp"
+
+/**
+ * @brief 实现 Soundex 类
+ *
+ */
+
+// TODO : 当前暂时与测试代码同一个文件，当感觉代码放在同一个文件有些麻烦时，再用合适的方式迁出
+
+class Soundex
+{
+};
+
+TEST_CASE("Retain sole letter of one letter world", "[SoundexEncoding]")
+{
+    GIVEN("A soundex var") { Soundex soundex; }
+}
