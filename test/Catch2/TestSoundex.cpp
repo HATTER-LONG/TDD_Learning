e5bfd20bc6c0b98dc1aa870fb6f1ac4e6917ab60
caolei <caolei6767@gmail.com>
2020-12-26 22:29:30 +0800

    实施新的编码规范

--- test/Catch2/TestSoundex.cpp
+++ test/Catch2/TestSoundex.cpp
@@ -1,17 +1,34 @@
 #include "catch2/catch.hpp"
 
-/**
- * @brief 实现 Soundex 类
- *
- */
+using namespace Catch;
 
 // TODO : 当前暂时与测试代码同一个文件，当感觉代码放在同一个文件有些麻烦时，再用合适的方式迁出
-
+/**
+ * @brief Soundex 算法类
+ *
+ */
 class Soundex
 {
+public:
+    /**
+     * @brief 按照 Soundex 算法转换输入的字符串内容
+     *
+     * @param word
+     * @return std::string
+     */
+    std::string encode(const std::string& Word) const { return ""; }
 };
 
 TEST_CASE("Retain sole letter of one letter world", "[SoundexEncoding]")
 {
-    GIVEN("A soundex var") { Soundex soundex; }
+    GIVEN("A soundex var")
+    {
+        Soundex soundex;
+
+        WHEN("Input one char")
+        {
+            auto encoded = soundex.encode("A");
+            THEN("Check that the return value is correct") { REQUIRE_THAT(encoded, Equals("A")); }
+        }
+    }
 }
