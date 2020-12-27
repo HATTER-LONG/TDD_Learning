e68b8076da03c14bfa84b5186e77e0ae84f959d5
caolei <caolei6767@gmail.com>
2020-12-27 23:15:19 +0800

    完善 Soundex 测试代码

--- test/Catch2/TestSoundex.cpp
+++ test/Catch2/TestSoundex.cpp
@@ -1,5 +1,5 @@
 #include "catch2/catch.hpp"
-
+#include "spdlog/spdlog.h"
 using namespace Catch;
 
 // TODO : 当前暂时与测试代码同一个文件，当感觉代码放在同一个文件有些麻烦时，再用合适的方式迁出
@@ -16,19 +16,44 @@ public:
      * @param word
      * @return std::string
      */
-    std::string encode(const std::string& Word) const { return ""; }
+    std::string encode(const std::string& Word) const { return zeroPad(Word); }
+
+private:
+    /**
+     * @brief 安装 Soundex 条款4 要求进行补零
+     *
+     * @param Word
+     * @return std::string
+     */
+    std::string zeroPad(const std::string& Word) const { return Word + "000"; }
 };
 
-TEST_CASE("Retain sole letter of one letter world", "[SoundexEncoding]")
+class FixtureSoundex
 {
-    GIVEN("A soundex var")
-    {
-        Soundex soundex;
+public:
+    Soundex MSoundex;
+};
 
+TEST_CASE_METHOD(FixtureSoundex, "Retain sole letter of one letter world", "[SoundexEncoding]")
+{
+    GIVEN("A MSoundex member")
+    {
         WHEN("Input one char")
         {
-            auto encoded = soundex.encode("A");
-            THEN("Check that the return value is correct") { REQUIRE_THAT(encoded, Equals("A")); }
+            auto encoded = MSoundex.encode("A");
+            THEN("Check that the return value is correct") { REQUIRE_THAT(encoded, Equals("A000")); }
+        }
+    }
+}
+
+TEST_CASE_METHOD(FixtureSoundex, "Pads with zeros to ensure three digits", "[SoundexEncoding]")
+{
+    GIVEN("A MSoundex member")
+    {
+        WHEN("Input one char")
+        {
+            auto encoded = MSoundex.encode("I");
+            THEN("Check that the return value pads with zeros") { REQUIRE_THAT(encoded, Equals("I000")); }
         }
     }
 }
