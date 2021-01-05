01f27539c879872ab9fe7414bb57103ec314c52b
caolei <caolei6767@gmail.com>
2021-01-05 23:05:26 +0800

    缩进问题修改

--- Src/Soundex/Soundex.cpp
+++ Src/Soundex/Soundex.cpp
@@ -43,14 +43,14 @@ void Soundex::encodeLetter(std::string& Encoding, char Letter, char LastLetter) 
 std::string Soundex::encodedDigit(char Letter) const
 {
     // clang-format off
-        const std::unordered_map<char, std::string> encodings { 
-            { 'b', "1" }, { 'f', "1" }, { 'p', "1" }, { 'v', "1" },
-            { 'c', "2" }, { 'g', "2" }, { 'j', "2" }, { 'k', "2" }, { 'q', "2" }, 
-                                        { 's', "2" }, { 'x', "2" }, { 'z', "2" },
-            { 'd', "3" }, { 't', "3" }, 
-            { 'l', "4" }, 
-            { 'm', "5" }, { 'n', "5" }, 
-            { 'r', "6" } };
+    const std::unordered_map<char, std::string> encodings { 
+        { 'b', "1" }, { 'f', "1" }, { 'p', "1" }, { 'v', "1" },
+        { 'c', "2" }, { 'g', "2" }, { 'j', "2" }, { 'k', "2" }, { 'q', "2" }, 
+                                    { 's', "2" }, { 'x', "2" }, { 'z', "2" },
+        { 'd', "3" }, { 't', "3" }, 
+        { 'l', "4" }, 
+        { 'm', "5" }, { 'n', "5" }, 
+        { 'r', "6" } };
     // clang-format on
 
     auto it = encodings.find(charutil::lower(Letter));
