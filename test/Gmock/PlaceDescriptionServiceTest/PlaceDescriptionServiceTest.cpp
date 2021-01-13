71f934b009b744e4fd3d66d1a8d21792ad88471a
caolei <caolei6767@gmail.com>
2021-01-13 22:39:08 +0800

    使用测试替身时提升测试的抽象程度

--- test/Gmock/PlaceDescriptionServiceTest/PlaceDescriptionServiceTest.cpp
+++ test/Gmock/PlaceDescriptionServiceTest/PlaceDescriptionServiceTest.cpp
@@ -19,29 +19,32 @@ const string APlaceDescriptionService::ValidLongitude("-104.44");
 
 class HttpStub : public Http
 {
+public:
+    string ReturnResponse;
+    string ExpectedURL;
     void initialize() override { }
     std::string get(const std::string& Url) const override
     {
         verify(Url);
-        return R"({ "address": {
-         "road":"Drury Ln",
-         "city":"Fountain",
-         "state":"CO",
-         "country":"US" }})";
+        return ReturnResponse;
     }
 
-    void verify(const string& Url) const
-    {
-        string urlStart("http://open.mapquestapi.com/nominatim/v1/reverse?format=json&");
-        auto expectedArgs(urlStart + "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
-                          "lon=" + APlaceDescriptionService::ValidLongitude);
-        ASSERT_THAT(Url, Eq(expectedArgs));
-    }
+    void verify(const string& Url) const { ASSERT_THAT(Url, Eq(ExpectedURL)); }
 };
 
 TEST_F(APlaceDescriptionService, ReturnsDescriptionForValidLocation)
 {
     HttpStub httpStub;
+    httpStub.ReturnResponse = R"({"address": {
+                                    "road":"Drury Ln",
+                                    "city":"Fountain",
+                                    "state":"CO",
+                                    "country":"US" }})";
+
+    string urlStart { "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&" };
+    httpStub.ExpectedURL = urlStart + "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
+                           "lon=" + APlaceDescriptionService::ValidLongitude;
+
     PlaceDescriptionService service { &httpStub };
     auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
     ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
