74290eccd1b383da76082716ac070f7b06609a28
caolei <caolei6767@gmail.com>
2021-01-13 23:28:31 +0800

    GMOCK 使用

---
+++ test/Gmock/PlaceDescriptionServiceTestByGMock/PlaceDescriptionServiceTest.cpp
@@ -0,0 +1,44 @@
+#include "Http.h"
+#include "PlaceDescriptionService/PlaceDescriptionService.h"
+
+#include "gmock/gmock.h"
+#include <gmock/gmock-generated-function-mockers.h>
+#include <gmock/gmock-matchers.h>
+
+using namespace std;
+using namespace testing;
+
+class APlaceDescriptionService : public Test
+{
+public:
+    static const string ValidLatitude;
+    static const string ValidLongitude;
+};
+
+const string APlaceDescriptionService::ValidLatitude("38.005");
+const string APlaceDescriptionService::ValidLongitude("-104.44");
+
+class HttpStub : public Http
+{
+public:
+    MOCK_METHOD(void, initialize, (), (override));
+    MOCK_CONST_METHOD1(get, string(const string&));
+};
+
+TEST_F(APlaceDescriptionService, ReturnsDescriptionForValidLocation)
+{
+    /*     HttpStub httpStub;
+         httpStub.ReturnResponse = R"({"address": {
+                                         "road":"Drury Ln",
+                                         "city":"Fountain",
+                                         "state":"CO",
+                                         "country":"US" }})";
+
+         string urlStart { "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&" };
+         httpStub.ExpectedURL = urlStart + "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
+                                "lon=" + APlaceDescriptionService::ValidLongitude;
+
+         PlaceDescriptionService service { &httpStub };
+         auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
+         ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));*/
+}
